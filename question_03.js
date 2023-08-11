0e2997070cd924d1f67e842aa599e57ef0fbad10
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 00:21:52 +0500

    question 3 code added

--- question_03.js
+++ question_03.js
@@ -1,2 +1,6 @@
 // Create an array called `numbers` with some numeric values. Use a `for...in` loop to
 // iterate over the array and log each index.
+const numbers =[24,675,14,7,356];
+for (num in numbers){
+    console.log(num)
+}
