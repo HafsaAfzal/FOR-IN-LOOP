fec42f10e2d4854350bd384f01f1b4e5c2d2295b
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 23:51:25 +0500

    question 2 code added

--- question_02.js
+++ question_02.js
@@ -1,2 +1,10 @@
 // Create an object called `person` with properties like `name`, `age`, and `country`.
 // Use a `for...in` loop to iterate over the object and log each property name.
+const person ={
+    name:"Hafsa",
+    age:20,
+    country:"Pakistan"
+}
+for (item in person){
+    console.log(item);
+}
