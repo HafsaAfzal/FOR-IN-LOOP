cb8be5f4b3a69e0b42e54846cc95b467a66ef420
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 00:29:46 +0500

    initiall commit

---
+++ question_05.js
@@ -0,0 +1,2 @@
+// Create a Map called `scores` with keys as names and values as scores. Use a `for...in`
+// loop to iterate over the Map and log each name and score pair.
---
+++ question_06.js
@@ -0,0 +1,2 @@
+// Create an object called `car` with properties like `brand`, `model`, and `year`. Use a
+// `for...in` loop to iterate over the object and log each property value.
