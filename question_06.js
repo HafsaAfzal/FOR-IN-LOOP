9c0d3c535d400f65046057a7a1221cbf82fb9e5c
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 00:32:43 +0500

    question 6 code added

--- question_06.js
+++ question_06.js
@@ -1,2 +1,10 @@
 // Create an object called `car` with properties like `brand`, `model`, and `year`. Use a
 // `for...in` loop to iterate over the object and log each property value.
+const car ={
+    brand:"unique",
+    model:"moz-34",
+    year:2024
+}
+for (prop in car ){
+    console.log(prop);
+}
