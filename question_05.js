86a17e1baca8de1b9c620581163f9de28fa4d58e
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-12 10:07:11 +0500

    code of question 5 added

--- question_05.js
+++ question_05.js
@@ -1,2 +1,9 @@
 // Create a Map called `scores` with keys as names and values as scores. Use a `for...in`
 // loop to iterate over the Map and log each name and score pair.
+const scores= new Map();
+scores.set("Hafsa",90);
+scores.set("Horrain",85);
+scores.set("Hareem",94);
+for ( let [items,value] in scores){         // for in loop does not iterate the map
+    console.log( `Name= ${items} scores=${value}`);
+}
